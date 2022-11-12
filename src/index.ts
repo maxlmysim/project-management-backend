a4578ad6a99058fd48f42712c7929bc5da2d1b7f
Maksim Lapko <94135700+maxlmysim@users.noreply.github.com>
2022-11-12 16:48:24 +0300

    fix: change user mongo

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import * as serverService from './services/server.service';
 
 (async () => {
   try {
-    await mongoose.connect('mongodb+srv://maxlmysimm:477075makc@cluster0.zudijjs.mongodb.net/?retryWrites=true&w=majority');
+    await mongoose.connect('mongodb+srv://maxlmysimm:ZPOI4afNQboAZNm9@cluster0.zudijjs.mongodb.net/?retryWrites=true&w=majority');
 
     serverService.server.listen(process.env.PORT || PORT, function () {
       console.log('Сервер ожидает подключения...');
